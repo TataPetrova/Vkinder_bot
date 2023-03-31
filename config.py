93b361aa2b3d8518ad33d6acbee9f81fe100d254
TataPetrova <112507389+TataPetrova@users.noreply.github.com>
2023-03-31 23:45:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,13 @@ import configparser
 
 # Объект парсера
 config = configparser.ConfigParser()
-
 if len(config.read("settings.ini")) == 0:
-    print('Добавьте файл setings.ini\nПример файла -> example/settings.ini')
+    print('Добавьте в файл settings.ini токен пользователя и токен группы, а так же данные postgresql')
     exit()
 
 # Токен пользователя
 user_token = config.get("settings", "user_token")
 # Токен группы
 group_token = config.get("settings", "group_token")
-# Файл для логгирования
-logging_file = config.get("settings", "logging_file")
 # База данных
 connstr = config.get("database", "connstr")
